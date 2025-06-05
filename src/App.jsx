948f07468395fa75f80eecc9abc447a5b0f377c7
KirkKang <x963842@gmail.com>
2025-06-06 01:05:11 +0800

    圖片路徑

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ console.log("App 檢查 auth 狀態：", authState);
   
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/GP">
     <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
