369fc687ead70e7cf7d11b3b4fb8d20acd1be5a9
KirkKang <x963842@gmail.com>
2025-06-06 14:54:10 +0800

    圖片

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -20,68 +20,68 @@ export const Categories =[
     "其他",
 ];
 
-export const mockData = [
-    {
-        id:1,
-        image: Image9,
-        name: "Product 1",
-        price: 30,
-    },
-    {
-        id:2,
-        image: Image8,
-        name: "Product 2",
-        price: 40,
-    },
-    {
-        id:3,
-        image: Image7,
-        name: "Product 3",
-        price: 50,
-    },
-    {
-        id:4,
-        image: Image6,
-        name: "Product 4",
-        price: 60,
-    },
-    {
-        id:5,
-        image: Image5,
-        name: "Product 5",
-        price: 70,
-    },
-    {
-        id:6,
-        image: Image4,
-        name: "Product 6",
-        price: 80,
-    },
-    {
-        id:7,
-        image: Image3,
-        name: "Product 7",
-        price: 90,
-    },
-    {
-        id:8,
-        image: Image2,
-        name: "Product 8",
-        price: 100,
-    },
-    {
-        id:9,
-        image: Image1,
-        name: "Product 9",
-        price: 110,
-    },
-    {
-        id:10,
-        image: Image10,
-        name: "Product 10",
-        price: 120,
-    },
-];
+// export const mockData = [
+//     {
+//         id:1,
+//         image: Image9,
+//         name: "Product 1",
+//         price: 30,
+//     },
+//     {
+//         id:2,
+//         image: Image8,
+//         name: "Product 2",
+//         price: 40,
+//     },
+//     {
+//         id:3,
+//         image: Image7,
+//         name: "Product 3",
+//         price: 50,
+//     },
+//     {
+//         id:4,
+//         image: Image6,
+//         name: "Product 4",
+//         price: 60,
+//     },
+//     {
+//         id:5,
+//         image: Image5,
+//         name: "Product 5",
+//         price: 70,
+//     },
+//     {
+//         id:6,
+//         image: Image4,
+//         name: "Product 6",
+//         price: 80,
+//     },
+//     {
+//         id:7,
+//         image: Image3,
+//         name: "Product 7",
+//         price: 90,
+//     },
+//     {
+//         id:8,
+//         image: Image2,
+//         name: "Product 8",
+//         price: 100,
+//     },
+//     {
+//         id:9,
+//         image: Image1,
+//         name: "Product 9",
+//         price: 110,
+//     },
+//     {
+//         id:10,
+//         image: Image10,
+//         name: "Product 10",
+//         price: 120,
+//     },
+// ];
 
 // // mockData.jsx
 // export const formatProductData = (data) => {
