845e9be79f517e8fcb463cb1e95a54ff6d3b98a8
KirkKang <x963842@gmail.com>
2025-06-07 02:00:41 +0800

    不浮動

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -35,7 +35,8 @@ const InfoSection = () => {
       <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4'>
         {infoItems.map((item,index)=>(
           <div key={index} className='flex flex-col items-center text-center p-4 border rounded-lg shadow-md
-          transform transition-transform duration-300 hover:scale-105 cursor-pointer'>
+          transform transition-transform duration-300 '>
+            {/* hover:scale-105 cursor-pointer */}
             {item.icon}
             <h3 className='mt-4 text-xl font-semibold'>{item.title}</h3>
             <p className='mt-2 text-gray-600'>{item.description}</p>
