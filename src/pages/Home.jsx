67bd4f37a3a78619be27355b880167661ea33142
KirkKang <x963842@gmail.com>
2025-06-07 02:31:19 +0800

    刪左邊

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
   <div>
     <div className='bg-white mt-2 px-4 md:px-16 lg:px-24'>
       <div className='container mx-auto py-4 flex flex-col md:flex-row space-x-2'>
-        <div className='w-full md:w-3/12'>
+        {/* <div className='w-full md:w-3/12'>
             <div className='bg-red-600 text-white text-xs font-bold px-2 py-2.5'>商店的類別</div>
             <ul className='space-y-4 bg-gray-100 p-3 border'>  
               {Categories.map((category, index)=>(
@@ -49,7 +49,7 @@ const Home = () => {
                 </li>
               ))}
             </ul>
-        </div>
+        </div> */}
         
         <div className='w-full md:9/12 mt-8 md:mt-0 h-96 relative'>
             <img src={HeroImage} alt="" className="h-full w-full"></img>
