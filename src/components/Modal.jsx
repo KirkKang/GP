74f2992c1e2cef68f5ab9d99e8378bfed0da1a4e
KirkKang <x963842@gmail.com>
2025-06-08 05:30:31 +0800

    手機頁面登入登出5(改尺寸)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 const Modal = ({ isModelOpen, setIsModelOpen, children }) => {
   if (!isModelOpen) return null;
 
   return (
-    <div className='fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50'>
-      <div className='relative bg-white rounded-lg shadow-lg p-6 w-[90%] max-w-md sm:p-6 p-4'>
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+      <div className="relative bg-white rounded-lg shadow-lg container mx-auto px-4 md:px-16 lg:px-24 py-6 max-h-[90vh] overflow-y-auto">
         <button
-          className='absolute top-2 right-4 text-gray-400 text-2xl'
+          className="absolute top-2 right-4 text-gray-500 text-3xl"
           onClick={() => setIsModelOpen(false)}
         >
           &times;
@@ -16,6 +14,4 @@ const Modal = ({ isModelOpen, setIsModelOpen, children }) => {
       </div>
     </div>
   );
-}
-
-export default Modal
+};
