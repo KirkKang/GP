561bdbe418381fdced44418c12ad137048f107f4
KirkKang <x963842@gmail.com>
2025-06-08 22:55:57 +0800

    搜尋後可以傳給別人

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import ProductCard from '../components/ProductCard'
 import { FaLaptop, FaCouch, FaBook, FaBoxes, FaTshirt, FaPuzzlePiece, FaTv, FaRunning } from 'react-icons/fa'
 import { Categories } from '../assets/mockData'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { setSearchTerm } from '../redux/productSlice'
+
+
+
+
 
 const categoryIcons = {
   "3C": <FaLaptop size={24} />,
@@ -16,17 +22,39 @@ const categoryIcons = {
 }
 
 const Shop = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const params = new URLSearchParams(location.search);
+    const searchFromURL = params.get("search");
+
+    useEffect(() => {
+      if (searchFromURL) {
+        dispatch(setSearchTerm(searchFromURL));
+      }
+    }, [searchFromURL, dispatch]);
+
     const products = useSelector(state => state.product)
     const [selectedCategory, setSelectedCategory] = useState(null)
+
+    const searchTerm = products.searchTerm?.toLowerCase() || "";
+
 
     const filteredProducts = products.products
     .filter(product => product.Shelf_status !== 0)
     .filter(product => !selectedCategory || product.Type === selectedCategory)
-
+    .filter(product =>
+    product.Product_name.toLowerCase().includes(searchTerm)
+  );
   return (
       <div className='mx-auto py-12 px-4 md:px-16 lg:px-24'>
         <h2 className='text-2xl font-bold mb-6 text-center'>商品</h2>
-
+          {searchTerm && (
+  <p className="text-center text-gray-500 mb-4">
+    搜尋結果：「{searchTerm}」
+  </p>
+)}
 
         {selectedCategory && (
         <p className="text-center text-lg font-medium mb-4">
@@ -37,7 +65,12 @@ const Shop = () => {
         {Categories.map(category => (
           <button
             key={category}
-            onClick={() => setSelectedCategory(category === selectedCategory ? null : category)}
+            onClick={() =>{ const newCategory =  category === selectedCategory ? null : category;
+              setSelectedCategory(newCategory);
+              navigate('/shop');
+              dispatch(setSearchTerm(""));
+            }}
+
             className={`flex flex-col items-center p-4 border rounded-xl transition 
               ${selectedCategory === category ? 'bg-red-500 text-white' : 'bg-white text-black hover:bg-red-100'}`}
           >
